457b1f1d4f7774a47f74749abc8b236e781e1266
Tiberio Brasil <tiberiobrasil@gmail.com>
2020-06-01 10:48:39 -0300

    Finalizado codigo do desafio

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -2,9 +2,24 @@ import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
 import CreateProductService from '@modules/products/services/CreateProductService';
+import AppError from '@shared/errors/AppError';
 
 export default class ProductsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    try {
+      const { name, price, quantity } = request.body;
+
+      const createProduct = container.resolve(CreateProductService);
+
+      const product = await createProduct.execute({
+        name,
+        price,
+        quantity,
+      });
+
+      return response.json(product);
+    } catch (err) {
+      throw new AppError(err.message, 400);
+    }
   }
 }
